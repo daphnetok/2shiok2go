8accac07c3b5dd671dbbfcc75bb8b56dacd7ad46
daphnetok <dtpz2727@gmail.com>
2025-09-29 22:56:19 +0800

    Added vue init files and implemented Dashboard page with 1 working bar chart

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,27 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'DashboardChart',
+  components: { Bar },
+  props: {
+    chartData: { type: Object, required: true },
+    chartOptions: { type: Object, required: false }
+  }
+}
+</script>
+
+<template>
+  <Bar :data="chartData" :options="chartOptions" />
+</template>
