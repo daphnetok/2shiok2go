5be83caf23e57013783c50e8e2355f5a2692e96e
Nicholas Wong <nicholas.w.2024@computing.smu.edu.sg>
2025-09-30 18:24:28 +0800

    updated auth to use vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Test v1.1</h1>
+    <img src="/funny-cat.jpg" width="200px" height="200px"></img>
+    <AuthComponent />
+  </div>
+</template>
+
+<script setup>
+import AuthComponent from './components/AuthComponent.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHAwM9yxLM34CX7_XjJZSrnDo2itLjU7M",
+  authDomain: "test-25bd6.firebaseapp.com",
+  projectId: "test-25bd6",
+  storageBucket: "test-25bd6.firebasestorage.app",
+  messagingSenderId: "857367460717",
+  appId: "1:857367460717:web:7523d546d003782bd0b864",
+  measurementId: "G-06XTM851DJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export auth and db instances
+export const auth = getAuth(app);
+export const db = getFirestore(app);
