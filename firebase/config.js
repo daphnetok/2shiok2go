d186fa5fd9f4ae065c0fc43d397ed6149a3dd28d
Nicholas Wong <nicholas.w.2024@computing.smu.edu.sg>
2025-10-12 11:55:34 +0800

    added storage service, fixed create listing logic

--- firebase/config.js
+++ firebase/config.js
@@ -4,6 +4,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHAwM9yxLM34CX7_XjJZSrnDo2itLjU7M",
@@ -18,6 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export auth and db instances
+// Initialize services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
